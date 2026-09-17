2ffbbd8e4340b90c13628c9c9a26e83b16a79bb5
farcydev <farcydev@gmail.com>
2026-09-18 00:01:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -244,7 +244,6 @@ displayCir()
       odser.observe(heade)
       odser.observe(foo)
     }
-    alert(cards.length);       alert(sectionE.children.length);
   }
   let odser = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
